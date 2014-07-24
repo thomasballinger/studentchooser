3a4e60d00dbad1201ff023349c4623040146c3ea
maianess <maia.mcc@gmail.com>
2014-07-24 15:58:59 -0400

    now ignores absent students when picking

--- studentchooser.py
+++ studentchooser.py
@@ -1,36 +1,54 @@
 from random import random
+from sys import exit
 
-prob_change = 0.05
+# change this value to adjust by how much a student's prob changes when picked
+prob_change = 3.0/4.0
+
+def ask():
+	return raw_input("> ")
+
 def pick_kid():
-	value = random()
+	value = random() * 100
 	startpoint = 0
 	endpoint = 0
-
-	for kid in students:
-		endpoint += roster[kid]
+	
+	for kid in present_students:
+		endpoint += present_students[kid].prob
 		#print "(%f, %f)" % (startpoint, endpoint)
-		if startpoint <= value <= endpoint:
+		if (startpoint <= value <= endpoint):
 			chosen = kid
-			if roster[kid] - prob_change < 0:
-				roster[kid] = 0
-			else:
-				roster[kid] -= prob_change
+			present_students[kid].picked += 1
 			break
 		else:
-			startpoint += roster[kid]
+			startpoint += present_students[kid].prob
 
 	return chosen
 
-
 def scale():
-	total = sum(roster.values())
+	# set all prob's back to scale to 100
 	for kid in roster:
-		roster[kid] *= 1.0 / total
+		roster[kid].prob = 100 * prob_change ** (roster[kid].picked)
+		# print roster[kid].name, roster[kid].prob
+	# for all kids who are present, adjust prob. by # of times picked, then scale
+	total = 0
+	for kid in present_students:
+			total += present_students[kid].prob
+	for kid in present_students:
+			present_students[kid].prob *= 100.0 / total
+
+def take_attendance():
+	present_kids = {}
+	for kid in roster:
+		if not(roster[kid].absent):
+			present_kids[kid] = roster[kid]
+	return present_kids
 
 def select():
 	the_student = pick_kid()
 	scale()
 	print the_student
+	print roster
+	print "----------"
 	return the_student
 
 # TESTING FUNCTIONS
@@ -52,16 +70,56 @@ def test_never(kid):
 def multi_test(x):
 	for i in range(0,x):
 		print "%d: %s" % (i, select())
+		#print roster
+		#print "-----------"
 
-students = ["ali", "ben", "chuck", "dave", "erin", "samer", "eric", "merlin", "arthur"]
-#roster = {"ali": 1, "ben": 1, "chuck": 1, "dave": 1, "erin": 1}
+# make a debug function that will display all of the prob's
+
+# how do i change how a Student object prints?
+class Student(object):
+	def __init__(self, name):
+		self.name = name
+		self.absent = False
+		self.prob = 1
+		self.picked = 0
+	def __repr__(self):
+		return "<%s, %f, absent = %d>" % (self.name, self.prob, self.absent)
+	def __str__(self):
+		return "<%s, %f, absent = %d>" % (self.name, self.prob, self.absent)
+
+# list of students (later, will get this from user input)
+students = ["ali", "ben", "chuck", "dave"]
+#, "erin", "samer", "eric", "merlin", "arthur", "rachel", "carlos", "sindhu"]
+
+# make an empty dict.
 roster = {}
+present_students = {}
 
+# populate that dict. with all students equally weighted
 for kid in students:
-	roster[kid] = 1
+	roster[kid] = Student(kid)
 
+# make a list of students who are present
+present_students = take_attendance()
+
+# scale the probabilities of the present students
 scale()
-print roster
+
+# for debugging
+print "Roster:", roster
+print "Present:", present_students
+
+while True:
+	print "1. pick, 2. input absences, 3. exit"
+	answer = ask()
+	if answer == "1":
+		select()
+	elif answer == "2":
+		print "You picked #2!"
+	elif answer == "3":
+		exit()
+	else:
+		print "Sorry, I didn't get that. Try again."	
 
 
 
